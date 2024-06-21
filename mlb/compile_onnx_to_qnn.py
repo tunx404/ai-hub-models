e5f94d40995a3cce9ec829b26e932d905c3734f1
tunx404 <tunx404@gmail.com>
2024-06-21 17:27:22 +0700

    Updated dataset

--- mlb/compile_onnx_to_qnn.py
+++ mlb/compile_onnx_to_qnn.py
@@ -42,3 +42,11 @@ compile_job = hub.submit_compile_job(
     options="--target_runtime qnn_context_binary --quantize_full_type w4a16 --truncate_64bit_io true --quantize_io true",
 )
 assert isinstance(compile_job, hub.CompileJob)
+
+# --target_runtime qnn_context_binary --quantize_full_type w4a16 --truncate_64bit_io true --quantize_io true
+# https://app.aihub.qualcomm.com/jobs/jn5qwmvo5/
+# [ ERROR ] Given input file /tmp/fee7e5ac-480d-4a87-99a7-f1ce88ec55f6h7z19d7o/tmpjchebw9d/inputs0_0.raw with file size in bytes 8. If the model expects a batch size of one, the file size should match the tensor extent: 4 bytes. If the model expects a batch size > 1, the file size should evenly divide the tensor extent: 4 bytes.
+
+# --target_runtime qnn_context_binary --quantize_full_type w4a16 --truncate_64bit_io true --quantize_io false
+# https://app.aihub.qualcomm.com/jobs/j1pv49drp/
+# [ERROR] Extracted input contains an unsupported input dtype: QNN_DATATYPE_INT_64
--- mlb/dataset.py
+++ mlb/dataset.py
@@ -1,34 +1,72 @@
 import numpy as np
 
+import os
+import shutil
+
 ####################
 
 data_l32_h32_part1 = {
-    'input_ids': [np.random.randint(0, 32000, (1, 1)).astype(np.int32)],
-    'attention_mask': [np.random.randint(0, 65536, (1, 1, 1, 1024)).astype(np.uint16)],
-    'position_ids_cos': [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
-    'position_ids_sin': [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)]
+    "input_ids": [np.random.randint(0, 32000, (1, 1)).astype(np.int32)],
+    "attention_mask": [np.random.randint(0, 65536, (1, 1, 1, 1024)).astype(np.uint16)],
+    "position_ids_cos": [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
+    "position_ids_sin": [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
 }
 for layer in range(8):
     for head in range(32):
-        data_l32_h32_part1[f"past_key_{layer}_h{head}"] = [np.random.randint(0, 256, (1, 1, 128, 1023)).astype(np.uint8)]
+        data_l32_h32_part1[f"past_key_{layer}_h{head}"] = [
+            np.random.randint(0, 256, (1, 1, 128, 1023)).astype(np.uint8)
+        ]
     for head in range(32):
-        data_l32_h32_part1[f"past_value_{layer}_h{head}"] = [np.random.randint(0, 256, (1, 1, 1023, 128)).astype(np.uint8)]
+        data_l32_h32_part1[f"past_value_{layer}_h{head}"] = [
+            np.random.randint(0, 256, (1, 1, 1023, 128)).astype(np.uint8)
+        ]
 
 ####################
 
 data_l32_h12 = {
-    'input_ids': [np.random.randint(0, 32000, (1, 1)).astype(np.int32)],
-    'attention_mask': [np.random.randint(0, 65536, (1, 1, 1, 1024)).astype(np.uint16)],
-    'position_ids_cos': [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
-    'position_ids_sin': [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)]
+    "input_ids": [np.random.randint(0, 32000, (1, 1)).astype(np.int32)],
+    "attention_mask": [np.random.randint(0, 65536, (1, 1, 1, 1024)).astype(np.uint16)],
+    "position_ids_cos": [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
+    "position_ids_sin": [np.random.randint(0, 65536, (1, 1, 1, 64)).astype(np.uint16)],
 }
 for layer in range(32):
     for head in range(12):
-        data_l32_h12[f"past_key_{layer}_h{head}"] = [np.random.randint(0, 256, (1, 1, 128, 1023)).astype(np.uint8)]
+        data_l32_h12[f"past_key_{layer}_h{head}"] = [
+            np.random.randint(0, 256, (1, 1, 128, 1023)).astype(np.uint8)
+        ]
     for head in range(12):
-        data_l32_h12[f"past_value_{layer}_h{head}"] = [np.random.randint(0, 256, (1, 1, 1023, 128)).astype(np.uint8)]
+        data_l32_h12[f"past_value_{layer}_h{head}"] = [
+            np.random.randint(0, 256, (1, 1, 1023, 128)).astype(np.uint8)
+        ]
 
 ####################
 
+
+def dump_dataset(dataset_dict: dict, output_dir: str, use_float: bool = False):
+    if use_float:
+        output_dir += "_float"
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "inputs"), exist_ok=True)
+
+    file_paths = []
+    for name, data_list in dataset_dict.items():
+        array = data_list[0]
+        if use_float:
+            array = array.astype(np.float32)
+        print(name)
+        file_path = os.path.join(output_dir, "inputs", f"{name}.npy")
+        np.save(file_path, array)
+        file_paths.append(file_path)
+
+    file_list_path = os.path.join(output_dir, "file_paths.txt")
+    with open(file_list_path, "w") as f:
+        f.write(" ".join(file_paths))
+
+
 if __name__ == "__main__":
-    print(f"data_l32_h32_part1: {[key for key in data_l32_h32_part1]}")
+    # print(f"data_l32_h32_part1: {[key for key in data_l32_h32_part1]}")
+    # dump_dataset(data_l32_h32_part1, "data_l32_h32_part1", use_float=True)
+    # dump_dataset(data_l32_h32_part1, "data_l32_h12")
+    pass
